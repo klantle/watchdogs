2ebbd2a7fb7566d5e820bd298591ee57d43a33a8
void <watchdogs@noreply.com>
2025-10-21 23:54:58 +0700

    -improved

--- hardware.h
+++ hardware.h
@@ -1,18 +1,13 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
-#ifdef _WIN32
 void hardware_cpu_info();
 void hardware_memory_info();
 void hardware_disk_info();
 void hardware_network_info();
 void hardware_system_info();
-#else
-void hardware_cpu_info();
-void hardware_memory_info();
-void hardware_disk_info();
-void hardware_network_info();
-void hardware_system_info();
+
+#ifndef _WIN32
 void hardware_bios_info();
 void hardware_gpu_info();
 #endif
